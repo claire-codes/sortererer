b007eac9cee030f06fb2c38c4452992359cd51ec
Claire Parker <claire.parker87@gmail.com>
2015-10-27 20:52:11 +0000

    :custard: No more jQuery huzzah!

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,38 +1,25 @@
-$(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
 	var sortListBtn = document.querySelector('#submit-list');
 
 	sortListBtn.addEventListener('click', function() {
-		var unsortedList = document.getElementById('unsorted-list-input').value;
-		var useAscii = document.getElementById('ascii').checked;
-		var useReverse = document.getElementById('reverse').checked;
-
-		$.post('/sortit', {
-			unsorted: unsortedList, 
-			ascii: useAscii, 
-			reverse: useReverse
-		})
-		.done(function(data) {
-			document.getElementById('sorted-list-output').value = data;
-			document.getElementById('result-text').innerHTML = 'Sorted!';
-		})
-		.fail(function(data) {
-			document.getElementById('result-text').innerHTML = 'Sort failed :(';
-		});
-	});
+		var unsortedList = encodeURI(document.getElementById('unsorted-list-input').value);
+		var useAscii = encodeURI(document.getElementById('ascii').checked);
+		var useReverse = encodeURI(document.getElementById('reverse').checked);
 		
-	// var request = new XMLHttpRequest();
-	// request.open('POST', '/sortit', true);
-	// request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	// request.onreadystatechange = function() {
-	// 	if(request.readyState == 4 && request.status == 200) {
-	// 		alert(request.responseText);
-	// 	}
-	// }
-	// request.send({
-	// 	unsorted: unsortedList, 
-	// 	ascii: useAscii, 
-	// 	reverse: useReverse
-	// });
+		var request = new XMLHttpRequest();
+		request.open('POST', '/sortit', true);
+		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 && request.status == 200) {
+				document.getElementById('sorted-list-output').value = request.responseText;
+				document.getElementById('result-text').innerHTML = 'Sorted!';
+			} else {
+				document.getElementById('result-text').innerHTML = 'Sort failed :(';
+			}
+		}
+		request.send('unsorted=' + unsortedList + '&ascii=' + useAscii + '&reverse=' + useReverse);
+		
+	});
 
 	var copyListBtn = document.querySelector('#copy-list');
 
@@ -45,7 +32,7 @@ $(function() {
 	    var msg = successful ? 'Copied!' : 'Copying is not supported';  
 	    document.getElementById('result-text').innerHTML = 'Copied!'; 
 	  } catch(err) {  
-	    console.log('Oops, unable to copy');  
+	    document.getElementById('result-text').innerHTML = 'Oops, unable to copy';  
 	  }  
 	});
 
